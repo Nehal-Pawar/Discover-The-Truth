7c8c3fa3142f6fc2ab1d628410632ed1907b406e
root <root@ip-10-0-0-45.us-west-2.compute.internal>
2020-02-11 06:53:18 +0000

    news from all files read

--- Data_ingestion/news_producer.py
+++ Data_ingestion/news_producer.py
@@ -14,24 +14,23 @@ import random
 import time
 import pandas as pd 
 import json
-
+import os
 def main():
-    data = pd.read_csv("/all_news_DB/home/articles1.csv") 
-    print(data.head())
-    dict_news=data.to_dict(orient='records')
-    #msg1=json.dumps(dict_news)
-    producer = KafkaProducer(bootstrap_servers = 'ip-10-0-0-37.us-west-2.compute.internal:9092,ip-10-0-0-12.us-west-2.compute.internal:9092,.internal:9092,ip-10-0-0-20.us-west-2.compute.internal:9092')
-    #producer.send('news',msg1)
-    i=0
-    for key in dict_news:
-	i=i+1
-	#if i > 3000:
-	#	break
-	producer.send('news',json.dumps(key))
-	#print(len(key))
-	#print(key)
-        producer.flush()
-	
+    entries = os.listdir('../../home/')
+    for entry in entries:
+        data = pd.read_csv("../../home/"+entry) 
+        print(data.head())
+        dict_news=data.to_dict(orient='records')
+        
+        producer = KafkaProducer(bootstrap_servers = 'ip-10-0-0-37.us-west-2.compute.internal:9092,ip-10-0-0-12.us-west-2.compute.internal:9092,.internal:9092,ip-10-0-0-20.us-west-2.compute.internal:9092')
+        #i=0
+        for key in dict_news:
+            #i=i+1
+            #if i > 3000:
+            #	break
+            producer.send('news',json.dumps(key))
+            producer.flush()
+            
 if __name__ == '__main__':
 
     main()
