b61b73fce52d60f7ce6230267d68fa286f08185f
root <root@ip-10-0-0-38.us-west-2.compute.internal>
2020-02-12 03:11:58 +0000

    website.py update final

--- app/website.py
+++ app/website.py
@@ -1,14 +1,17 @@
+# -*- coding:utf-8 -*-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from boto3.dynamodb.conditions import Key, Attr
 import boto3
+from dash_table import DataTable
+import dash_table
 import sys
 sys.path.insert(1, '/home/ubuntu/')
 
 from config import (aws_access_key, aws_secret_key)
-
-table_name="mytable5"
+PAGE_SIZE = 20
+table_name="mytable6"
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -16,51 +19,76 @@ server = app.server
 
 dynamodb = boto3.resource('dynamodb',aws_access_key_id=aws_access_key ,aws_secret_access_key= aws_secret_key,region_name='us-west-2')
 table = dynamodb.Table(table_name)
-#response = table.get_item(    Key={        'id':'123'    })
-
 #response = table.query(IndexName="keyword-rank-index", KeyConditionExpression=Key('keyword').eq('trump'))
+def getList(dict): 
+    return dict.keys() 
 
-#response = table.query(KeyConditionExpression=Key('keyword').eq('trump'))
-#item = response['Items']
-#print(item)
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-    
+response = table.query(KeyConditionExpression=Key('keyword').eq('2016 presidential election'))
+item = response['Items']
+#print(getList(item[0]))
+#print(item[0])
 
-    html.Div(children=""),
+column=['keyword','id','author','content','date','publication','title','topic','name','text']
+dtgood = dash_table.DataTable(
+
+    id='datatablegood',
+    style_data={
+
+        'whiteSpace': 'normal',
+
+        'height': 'auto'
+
+    },
+
+    columns=[{"name":str(i), "id":str(i)} for i in column],
+
+    data=response['Items']
+
+)
+
+app.layout = html.Div( children=[
+    dcc.Markdown('''News'''),
+    html.H1(children='Discover The Truth'),
     dcc.Dropdown(
 
 id='demo-dropdown',
 
 options=[
 
-{'label': '17362', 'value': '17362'},
+{'label': '2016 presidential election', 'value': '2016 presidential election'},
 
-{'label': '2', 'value': '17393'},
+{'label': 'gunman opens fire', 'value': 'gunman opens fire'},
 
-{'label': 'western-australia', 'value': 'western-australia'},
+{'label': '45th President', 'value': '45th President'},
 
-{'label': 'san-francisco', 'value': 'san-francisco'}
+{'label': 'Syrian Civil War', 'value': 'Syrian Civil War'},
+
+{'label': 'Chelsea Manning\'s', 'value': 'Chelsea Manning\'s'}
 
 ],
 
-value='san-francisco'
+#value='17362'
 
 ),
-    html.Div(id='dd-output-container')
+    html.Div(id='dd-output-container'),
+
+    dtgood 
+
 ])
 
 @app.callback(
 
-    dash.dependencies.Output('dd-output-container', 'children'),
+    dash.dependencies.Output('datatablegood', 'data'),
+    #dash.dependencies.Output('table_models', 'data'),
 
     [dash.dependencies.Input('demo-dropdown', 'value')])
 
 def updateoutput(value):
-    response = table.query(KeyConditionExpression=Key('id').eq(value))
-    item = response['Items']
-# Get the service resource.
-    return 'You have selected "{}"'.format(str(item))
+
+    response = table.query(KeyConditionExpression=Key('keyword').eq(value))
+    #item = response['Items']
+    # Get the service resource.
+    return response['Items']  #'You have selected "{}"'.format(listToStr)
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0',debug=True)
